c8ccdcba71ef880731ac6626711d76b24a89b842
PraveenReddy I <narayana.praveen143@gmail.com>
2022-02-12 17:16:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,15 +3,35 @@ provider "aws" {
   access_key = "AKIATIB5K2VXSX3F3TO5"
   secret_key = "U1PWDcCZKjcOTgVsu9kprD1jqmKsx9psuzxUWygn"
 }
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.b.id
-  acl    = "private"
+#1 -this will create a S3 bucket in AWS
+resource "aws_s3_bucket" "terraform_state_s3" {
+  #make sure you give unique bucket name
+  bucket = "terraform-teknobucket1-state" 
+  force_destroy = true
+# Enable versioning to see full revision history of our state files
+  versioning {
+         enabled = true
+        }
+
+# Enable server-side encryption by default
+server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-
-
+# 2 - this Creates Dynamo Table
+resource "aws_dynamodb_table" "terraform_locks" {
+# Give unique name for dynamo table name
+  name         = "TerraFom1-up-and-run-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+        attribute {
+         name = "LockID"
+         type = "S"
+      }
+}
 
