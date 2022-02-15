d7b9e205c1536da64650e9f8e91b4df466c813b7
PraveenReddy I <narayana.praveen143@gmail.com>
2022-02-15 10:35:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-  region     = "us-east-2"
-  access_key = "AKIATIB5K2VXSX3F3TO5"
-  secret_key = "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"
-}
 terraform {
   backend "s3" {
     bucket  = "delete-1"
