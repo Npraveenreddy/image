bbbbf6e414ace8b0616939c95483b00a6921a960
PraveenReddy I <narayana.praveen143@gmail.com>
2022-02-15 10:21:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,9 +7,4 @@ provider "aws" {
 resource "aws_s3_bucket" "terraform_state_s3" {
   #make sure you give unique bucket name
   bucket = "terraform-teknobucket1-state" 
-  force_destroy = true
-# Enable versioning to see full revision history of our state files
-  versioning {
-         enabled = true
-        }
 }
