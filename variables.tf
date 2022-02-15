dc0d0bec59a65163af1bfda41269b959488ecdeb
PraveenReddy I <narayana.praveen143@gmail.com>
2022-02-15 10:36:00 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,5 @@
-variable "region" {
-    default = "us-east-2"
+provider "aws" {
+  region     = "us-east-2"
+  access_key = "AKIATIB5K2VXSX3F3TO5"
+  secret_key = "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"
 }
-
-variable "instance_type" {
-    default = "t2.micro"
-} 
